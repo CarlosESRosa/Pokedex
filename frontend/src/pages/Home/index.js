618834c925308082660cf808dbbcd83528b9415e
Carlos Rosa <carlosrosa1001@gmail.com>
2022-12-15 15:09:00 -0300

    Tela de Home

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -7,16 +7,30 @@ import roseImg from "../../img/rose.png";
 
 const Home = () => {
   const [statePokemons, setStatePokemons] = useState([]);
+  const [statePokemonsCopy, setStatePokemonsCopy] = useState([]);
   const [stateUsername, setStateUsername] = useState("");
+  const [statePokemonSearch, setStatePokemonSearch] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
   const loadDatas = async () => {
     const pokemons = await getPokemons();
     const userLocalStorage = localStorage.getItem("user");
     setStatePokemons(pokemons);
+    setStatePokemonsCopy(pokemons);
     setStateUsername(userLocalStorage);
     setIsLoading(false);
   };
+
+  function handleChange(event, setState) {
+    setState(event.currentTarget.value);
+  }
+
+  function handleFilter() {
+    const filtredPokemons = statePokemonsCopy.filter(
+      (element) => element.name == statePokemonSearch
+    );
+    setStatePokemons(filtredPokemons);
+  }
 
   useEffect(() => {
     loadDatas();
@@ -46,6 +60,51 @@ const Home = () => {
           </div>
         </div>
       </nav>
+      <div className="search-box">
+        <div className="search-form">
+          <label htmlFor="pokemonSearch" className="form-label">
+            Search:
+          </label>
+          <input
+            type="text"
+            value={statePokemonSearch}
+            onChange={(event) => handleChange(event, setStatePokemonSearch)}
+            className="form-control"
+            id="pokemonSearch"
+          />
+        </div>
+        <div className="search-icon">
+          <i
+            className="fa-solid fa-magnifying-glass"
+            onClick={handleFilter}
+          ></i>
+        </div>
+      </div>
+      <div className="pokemons-grid">
+        {statePokemons.map((element) => {
+          return (
+            <div className="pokemon-card" key={element.id}>
+              <div>
+                <img src={element.image} alt="pokemon image"></img>
+              </div>
+              <p>{`N: ${element.id}`}</p>
+              <p>{element.name}</p>
+              <div>
+                {element.types.map((type) => {
+                  return (
+                    <div
+                      className={`type-${type.type} default-type`}
+                      key={Math.random()}
+                    >
+                      <p>{type.type}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
