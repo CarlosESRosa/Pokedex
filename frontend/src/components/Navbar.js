db6772984b860d340d7a85f7ef93cdc3425fecc4
Carlos Rosa <carlosrosa1001@gmail.com>
2023-01-03 07:27:11 -0300

    feat: :sparkles: Navbar component

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -11,24 +11,45 @@ const Navbar = (props) => {
   };
 
   return (
-    <nav className="navbar fixed-top my-navbar">
-      <img src={roseImg} alt="rose image"></img>
-      <div className="my-navbar-seeds">
-        <div className="nav-character">
-          <i className="fa-solid fa-user"></i>
-          <h4>{props.username}</h4>
+    <header className="header">
+      <nav class="navbar navbar-expand-md ">
+        <div class="container-fluid">
+          <a class="navbar-brand ms-2" href="#">
+            <img src={roseImg} alt="rose image"></img>
+          </a>
+          <button
+            class="navbar-toggler me-2"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link mx-1 fs-4" href="home">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link mx-1 fs-4" href="favorites">
+                  Favorites
+                </a>
+              </li>
+              <li>
+                <a class="nav-link mx-1 fs-4" href="" onClick={logout}>
+                  <i className="fa-solid fa-right-from-bracket"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div>
-          <h4 onClick={() => navigate("/home")}>Home</h4>
-        </div>
-        <div>
-          <h4 onClick={() => navigate("/favorites")}>Favorites</h4>
-        </div>
-        <div onClick={logout} className="logout-button">
-          <i className="fa-solid fa-right-from-bracket"></i>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 };
 
